a027d002f9cd9904285cc372f4c1dd9ee40e0bea
Pablitosilva2021 <89499068+Pablitosilva2021@users.noreply.github.com>
2025-03-03 17:36:40 -0300

    Add files via upload

--- 06-ReportesProcedures.sql
+++ 06-ReportesProcedures.sql
@@ -10,8 +10,7 @@
 use Com1353G06
 go
 
---Por rango de fechas: ingresando un rango de fechas a demanda, debe poder mostrar
---la cantidad de productos vendidos en ese rango, ordenado de mayor a menor
+--Por rango de fechas: ingresando un rango de fechas a demanda, debe poder mostrar la cantidad de productos vendidos en ese rango, ordenado de mayor a menor
 
 CREATE OR ALTER PROCEDURE Venta.Cantidad_RangoFechas
 @FechaInicio DATE,
@@ -31,9 +30,7 @@ BEGIN
 END;
 go
 
---Mostrar total acumulado de ventas (o sea también mostrar el detalle) para una fecha
---y sucursal particulares
-
+--Mostrar total acumulado de ventas (o sea también mostrar el detalle) para una fecha y sucursal particulares
 
 CREATE OR ALTER PROCEDURE Venta.TotalVentasporSucursal
 @Fecha DATE,
@@ -48,9 +45,9 @@ BEGIN
 		dv.Subtotal,
 		sum(dv.Subtotal) OVER() AS VentaTotal
 	FROM Venta.Sucursal s INNER JOIN Persona.Empleado e ON s.Id_Suc = e.Id_Suc
-	INNER JOIN Venta.Venta_Registrada vr ON vr.Id_Emp = e.Legajo
-	INNER JOIN Venta.Detalle_Venta dv ON dv.Id_Venta = vr.Id
-	INNER JOIN Articulo.Producto p ON p.Id_Prod = dv.Id_Prod
+		INNER JOIN Venta.Venta_Registrada vr ON vr.Id_Emp = e.Legajo
+		INNER JOIN Venta.Detalle_Venta dv ON dv.Id_Venta = vr.Id
+		INNER JOIN Articulo.Producto p ON p.Id_Prod = dv.Id_Prod
 	WHERE vr.Fecha = @Fecha and s.Localidad_Real = @Sucursal
 
 	FOR XML PATH('VentasPorSucursalyFecha'), ROOT('ReporteVentas');
@@ -63,15 +60,12 @@ CREATE OR ALTER PROCEDURE Venta.mensual_1
    @mes INT, @anio INT
 AS
 BEGIN
-
 		SELECT 
-			DATENAME(WEEKDAY, r.fecha) AS dia_semana,SUM(f.Monto) AS total_facturado
-		FROM Venta.Venta_Registrada r
-		JOIN Venta.Factura f ON r.Id_Fac = f.id
-		WHERE month(r.fecha) = @mes AND year(r.fecha) = @anio
-		GROUP BY DATENAME(WEEKDAY, r.fecha)
+			DATENAME(WEEKDAY, vr.fecha) AS dia_semana,SUM(vr.Monto) AS total_facturado
+		FROM Venta.Venta_Registrada vr
+		WHERE month(vr.fecha) = @mes AND year(vr.fecha) = @anio
+		GROUP BY DATENAME(WEEKDAY, vr.fecha)
 		FOR XML PATH('dia'), ROOT('facturacionMensual');
-
 END;
 go
 
@@ -80,13 +74,12 @@ go
 CREATE OR ALTER PROCEDURE Venta.Trimestral
 AS
 BEGIN
-	select Format(VR.Fecha,'MM') As Mes, E.Turno,
-		Sum(F.monto) as Total_Facturado
-		from venta.Venta_Registrada VR
-		join Persona.Empleado E on Vr.id_Emp = E.Legajo
-		join Venta.Factura F on Vr.Id_Fac = F.id
-	where F.Estado = 1
-		Group by Format (Vr.Fecha,'MM'), E.Turno
+	select Format(vr.Fecha,'MM') As Mes, E.Turno,
+		Sum(vr.monto) as Total_Facturado
+		from venta.Venta_Registrada vr
+		join Persona.Empleado E on vr.id_Emp = E.Legajo
+	where vr.Estado = 1
+		Group by Format (vr.Fecha,'MM'), E.Turno
 		Order by Mes, E.Turno
 	For XML PATH ('Factura'), ROOT ('Facturas');
 END;
@@ -105,14 +98,13 @@ BEGIN
 			e.Legajo,
 			CONVERT(VARCHAR(256), DecryptByPassPhrase(@FraseClave, e.Nombre)) + ' ' 
 			+ CONVERT(VARCHAR(256), DecryptByPassPhrase(@FraseClave, e.Apellido)) AS vendedor,
-			SUM(f.Monto) AS total_facturado,
-			RANK() OVER (PARTITION BY s.Id_Suc ORDER BY SUM(f.Monto) DESC) AS ranking
+			SUM(vr.Monto) AS total_facturado,
+			RANK() OVER (PARTITION BY s.Id_Suc ORDER BY SUM(vr.Monto) DESC) AS ranking
 		FROM Venta.Venta_Registrada vr
-		JOIN Persona.Empleado e ON vr.Id_Emp = e.Legajo
-		JOIN Venta.Sucursal s ON e.Id_Suc = s.Id_Suc
-		JOIN Venta.Factura f ON vr.Id_Fac = f.Id
+			JOIN Persona.Empleado e ON vr.Id_Emp = e.Legajo
+			JOIN Venta.Sucursal s ON e.Id_Suc = s.Id_Suc
 		WHERE MONTH(vr.Fecha) = @mes AND YEAR(vr.Fecha) = @anio
-		GROUP BY s.Id_Suc, s.Localidad_Real, e.Legajo, e.Nombre, e.Apellido
+			GROUP BY s.Id_Suc, s.Localidad_Real, e.Legajo, e.Nombre, e.Apellido
 	)
 	SELECT 
 		sucursal,
@@ -120,11 +112,10 @@ BEGIN
 		total_facturado
 	FROM RankingVendedores
 	WHERE ranking = 1
-	FOR XML PATH('sucursal'), ROOT('mejorVendedorPorSucursal');
+	FOR XML PATH('sucursal'), ROOT('MejorVendedorPorSucursal');
 
 END;
 go
-
 
 --Mostrar los 5 productos más vendidos en un mes, por semana
 
@@ -141,8 +132,8 @@ BEGIN
 			SUM(dv.Cantidad) AS Total_Vendido,
 			RANK() OVER (PARTITION BY DATEPART(WEEK, vr.Fecha) ORDER BY SUM(dv.Cantidad) DESC) AS ranking
 		FROM Venta.Venta_Registrada vr
-		JOIN Venta.Detalle_Venta dv ON vr.Id = dv.Id_Venta
-		JOIN Articulo.Producto p ON dv.Id_Prod = p.Id_Prod
+			JOIN Venta.Detalle_Venta dv ON vr.Id = dv.Id_Venta
+			JOIN Articulo.Producto p ON dv.Id_Prod = p.Id_Prod
 		WHERE MONTH(vr.Fecha) = @mes AND YEAR(vr.Fecha) = @anio
 		GROUP BY DATEPART(WEEK, vr.Fecha), dv.Id_Prod, p.Nombre
 	)
@@ -187,26 +178,26 @@ BEGIN
 
 END;
 go
+
 -- por rango de fechas: ingresando un rango de fechas a demanda, debe poder mostrarla cantidad de productos vendidos en ese rango por sucursal, ordenado de mayor amenor.
 
 CREATE OR ALTER PROCEDURE venta.fecha_1
    @fechaInicio DATE, @fechaFin DATE
 AS
 BEGIN
+	SELECT 
+		s.Localidad_Real AS Sucursal,
+		p.Nombre AS Producto,
+		SUM(dv.Cantidad) AS Cantidad_Vendida
+	FROM Venta.Venta_Registrada vr
+		JOIN Venta.Detalle_Venta dv ON vr.Id = dv.Id_Venta
+		JOIN Articulo.Producto p ON dv.Id_Prod = p.Id_Prod
+		JOIN Persona.Empleado e ON vr.Id_Emp = e.Legajo
+		JOIN Venta.Sucursal s ON e.Id_Suc = s.Id_Suc
+	WHERE vr.Fecha BETWEEN @fechaInicio AND @fechaFin
+		GROUP BY s.Localidad_Real, p.Nombre
+		ORDER BY Cantidad_Vendida DESC
+	FOR XML PATH('producto'), ROOT('productosVendidosPorSucursal');
 
-SELECT 
-    s.Localidad_Real AS Sucursal,
-    p.Nombre AS Producto,
-    SUM(dv.Cantidad) AS Cantidad_Vendida
-FROM Venta.Venta_Registrada vr
-	JOIN Venta.Detalle_Venta dv ON vr.Id = dv.Id_Venta
-	JOIN Articulo.Producto p ON dv.Id_Prod = p.Id_Prod
-	JOIN Persona.Empleado e ON vr.Id_Emp = e.Legajo
-	JOIN Venta.Sucursal s ON e.Id_Suc = s.Id_Suc
-WHERE vr.Fecha BETWEEN @fechaInicio AND @fechaFin
-	GROUP BY s.Localidad_Real, p.Nombre
-	ORDER BY Cantidad_Vendida DESC
-FOR XML PATH('producto'), ROOT('productosVendidosPorSucursal');
-
-END;
+	END;
 go
